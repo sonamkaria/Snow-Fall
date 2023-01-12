1efec4eca8adc4a420b67f0bcc7b2a69c799537d
Sonam Thakkar <sonamthakkar@sonams-mbp.lan>
2023-01-11 19:50:57 -0800

    frontend folders created

---
+++ project-three-frontend/src/App.js
@@ -0,0 +1,15 @@
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <Header/>
+     <Footer/>
+    </div>
+  );
+}
+
+export default App;
---
+++ project-three-frontend/src/components/Header.js
@@ -0,0 +1,9 @@
+function Header(props){
+    return (
+   <header>
+    Header
+   </header>
+    )
+}
+
+export default Header;
